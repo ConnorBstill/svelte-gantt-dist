3adec6f35dfb0817f7c0f26a4f9dd13fdbdf7b3d
ConnorBStill <connor@lamarsoftware.io>
2022-07-12 16:55:27 -0300

    Initial

---
+++ types/modules/table/tableHeader.d.ts
@@ -0,0 +1,12 @@
+import { SvelteRow } from "../../core/row";
+export interface TableHeader {
+    /** Table column title */
+    title: string;
+    /** Table row property */
+    property: string;
+    width?: number;
+    /** Result can be a html string */
+    renderer?: (row: SvelteRow) => string;
+    /** Type of header, can be `tree` */
+    type?: 'tree' | any;
+}
---
+++ types/utils/utils.d.ts
@@ -0,0 +1,27 @@
+export declare class GanttUtils {
+    from: number;
+    to: number;
+    width: number;
+    magnetOffset: number;
+    magnetUnit: string;
+    magnetDuration: number;
+    /** because gantt width is not always correct */
+    totalColumnDuration: number;
+    totalColumnWidth: number;
+    constructor();
+    /**
+     * Returns position of date on a line if from and to represent length of width
+     * @param {*} date
+     */
+    getPositionByDate(date: number): number;
+    getDateByPosition(x: any): number;
+    roundTo(date: number): number;
+}
+export declare function getPositionByDate(date: number, from: number, to: number, width: number): number;
+export declare function getDateByPosition(x: number, from: number, to: number, width: number): number;
+export declare function getIndicesOnly<T, C = number | Date>(input: T[], value: C, comparer: {
+    (T: T): C;
+}, strict?: boolean): number[];
+export declare function get<T, C = number | Date>(input: T[], value: C, comparer: {
+    (T: T): C;
+}, strict?: boolean): T[];
